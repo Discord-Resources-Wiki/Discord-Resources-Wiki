9fd22cf9e735594381cc3bafcfd66c248c47cc4e
DyDestroyer <42097667+DyDestroyer1027@users.noreply.github.com>
2022-01-06 21:29:54 -0500

    Moved meta tag fix to root.jsx of the theme file

--- src/theme/Root.jsx
+++ src/theme/Root.jsx
@@ -6,6 +6,7 @@ export default function Root({children}) {
         <>
             <Head>
                 <meta property="og:image" content="/img/logo-background.png" />
+                <meta property="twitter:card" content="summary" />
             </Head>
             {children}
         </>
