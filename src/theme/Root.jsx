215ae11036122a9a34bdf1e433d6d0c9526929a3
Abhigyan Tripathi <abhigyantrips@gmail.com>
2022-01-09 15:24:14 +0530

    feat(theme): Add `theme-color` meta attribute.
    This will enable the Discord embed for the link to have a purple color, matching the logo.

--- src/theme/Root.jsx
+++ src/theme/Root.jsx
@@ -6,6 +6,7 @@ export default function Root({children}) {
         <>
             <Head>
                 <meta property="og:image" content="/img/logo-background.png" />
+                <meta name="theme-color" content="#ba6ec4" />
             </Head>
             {children}
         </>
