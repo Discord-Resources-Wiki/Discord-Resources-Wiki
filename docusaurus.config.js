7e5ca3432d2cf129a8845099dcfe2688e41025b9
JantsoP <jantso.porali@gmail.com>
2022-05-19 22:58:23 +0300

    Fix dark mode as requested (#166)
    * Upload package-lock and yarn.lock
    
    Upload package-lock and yarn.lock
    
    * Fix dark mode
    
    Fixed dark mode for this version
    
    * Make dark mode as default
    
    Due to an bug in Docusaurus code, respectPrefersColorscheme will not work as standalone and will default always to light mode even when user has dark mode on by default. This will cause mode switcher to think, that you have light mode and even some website assets will default to light mode if you call to check what mode they are using.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
                 },
             ],
             colorMode: {
-                respectPrefersColorScheme: true
+                defaultMode: 'dark'
               },
             navbar: {
                 title: 'Discord Resources',
--- src/components/InviteWidget.jsx
+++ src/components/InviteWidget.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {
+    useColorMode,
+  } from '@docusaurus/theme-common';
 
 export default function InviteWidget({invite}) {
-
+    const {isDarkTheme} = useColorMode();;
     return (
         <a href={`https://discord.gg/${invite}`} target="_blank">
-            <img src={`https://invidget.switchblade.xyz/${invite}`} alt=""/>
+             <img src={`https://invidget.switchblade.xyz/${invite}?theme=${isDarkTheme ? 'dark' : 'light'}`} alt=""/>
         </a>
     )
 }
